a9ab923430251e2d661b7f1881001d05f13f759a
Alexa <alexa.anderson@outlook.com>
2017-03-17 19:41:42 -0500

    Better answer selection

--- index.js
+++ index.js
@@ -2,6 +2,6 @@ const server = require('./lib/server.js');
 const io = require('./lib/socket');
 
 // Constants
-var PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3000;
 
 server.listen(PORT, console.log(`listening on port ${PORT}`))
